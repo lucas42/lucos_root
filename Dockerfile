3f856008a047837f8abfe2a0b922391fd9d84156
Luke Blaney <git@lukeblaney.co.uk>
2022-06-24 01:58:41 +0100

    Make work offline again
    This time using Service Worker, rather than deprecated appcache.
    Fixes #19

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,6 @@ RUN echo "Include conf/vhost.conf" >> /usr/local/apache2/conf/httpd.conf
 COPY vhost.conf /usr/local/apache2/conf/
 COPY public/ /usr/local/apache2/lucos_root
 COPY --from=navbar lucos_navbar.js /usr/local/apache2/lucos_root/
+COPY sw-template.js /tmp/
+RUN sed '/urls = \[/r'<(ls  /usr/local/apache2/lucos_root/ | awk '{print "\t\"/"$1"\""}' ORS=',\n') /tmp/sw-template.js > /usr/local/apache2/lucos_root/serviceworker.js
+RUN rm /tmp/sw-template.js
---
+++ sw-template.js
@@ -0,0 +1,41 @@
+const STATIC_CACHE = 'static-v1';
+// Array generated in Dockerfile based on listing all files
+const localUrls = [
+	"/",
+];
+const crossDomainUrls = [
+	"https://seinn.l42.eu/logo.jpg",
+	"https://notes.l42.eu/icon.png",
+	"https://monitoring.l42.eu/icon",
+	"https://am.l42.eu/icon",
+	"https://app.tfluke.uk/img/icon.png",
+	"https://contacts.l42.eu/resources/logo-highres.png",
+	"https://loganne.l42.eu/icon",
+];
+async function install() {
+	try {
+		const cache = await caches.open(STATIC_CACHE);
+		await cache.addAll(localUrls);
+
+		// `addAll` doesn't work for URLs which need a `no-cors` request
+		// Instead need to fetch them individually and call `put`
+		Promise.all(crossDomainUrls.map(async url => {
+			const request = new Request(url, {mode: 'no-cors'})
+			const response = await fetch(request);
+			await cache.put(request, response);
+		}));
+	} catch (error) {
+		console.error("Failed to cache resources:", error.message);
+	}
+}
+self.addEventListener('install', event => {
+	event.waitUntil(install());
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith((async () => {
+		const cachedResponse = await caches.match(event.request);
+		if (cachedResponse) return cachedResponse;
+		else return fetch(event.request);
+	})());
+});
