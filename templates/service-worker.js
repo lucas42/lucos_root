7c0ce855400922753fd98374b2c93e4aa084e1c1
Luke Blaney <git@lukeblaney.co.uk>
2024-09-09 00:53:08 +0100

    upgrade logic for loading new service workers, to utilise the status indicator in navbar

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -27,3 +27,11 @@ self.addEventListener('fetch', event => {
 		else return fetch(event.request);
 	})());
 });
+
+const statusChannel = new BroadcastChannel("lucos_status");
+statusChannel.addEventListener("message", function statusMessage(event) {
+	switch (event.data) {
+		case "service-worker-skip-waiting":
+			self.skipWaiting();
+	}
+});
